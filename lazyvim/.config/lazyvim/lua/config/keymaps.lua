9af305526e8586890402449b3496109b956a2974
elzren <elzren00@gmail.com>
2026-08-20 17:57:46 +0530

    update

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("x", "p", [["_dP]], { desc = "Keep last yanked when pasting", noremap = true, silent = true })
